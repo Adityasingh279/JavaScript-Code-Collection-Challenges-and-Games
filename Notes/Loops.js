2aea95d21dc7f4f20100a37f15694914d11ae79c
Aditya Singh <aditya.singh27920@gmail.com>
2023-12-13 12:12:55 +0530

    loops
    loops

--- Notes/Loops.js
+++ Notes/Loops.js
@@ -1,1 +1,62 @@
 // Chapter 6: Loops
+
+// for loops
+const fruits = ["banana", "apple", "orange", "pears"];
+for (let i = 0; i < fruits.length; i++) {
+  console.log(fruits[i]);
+}
+//this is an obsolete loop method
+
+// short new method for loops
+const numbers=[1,2,3,4,5,6,7,8,9]
+for(const number of numbers){
+    console.log(number) //if you use numbers, it'll print the whole array 'number' times. so be careful
+}
+
+// letter calculator
+const howManyLetters = () => {
+    const Phrase = `Hey, can you go to grocery store with me?`
+    for(letter in Phrase){
+        console.log(letter)
+    }
+}
+howManyLetters()
+
+//or
+//for getting actual letters, do this
+for(const letter of Phrase)
+//for getting index of each one of those
+for(const letter in Phrase)
+
+//sum of numbers in an array
+const sumArray = (numbers) => {
+  let result = 0;
+  for (const number of numbers) {
+    result = result + number;
+  }
+  return result;
+};
+const numb = [1, 2, 3, 4, 5, 6, 7];
+res = sumArray(numb);
+console.log(res);
+
+//biggest number
+const Max = (numbers) => {
+    let result = numbers[0]
+    for(const number of numbers){
+    if(number>result){
+        result=number
+    }
+}
+return result
+}
+console.log(Max([1,2,3,4,5,6,7,20,5,7,975,348,2628,72,272372,983289]));
+
+//letter frequency
+const letterFrequency = (phrase) => {
+    let freq = 0;
+    for(word of phrase){
+        
+    }
+
+}
