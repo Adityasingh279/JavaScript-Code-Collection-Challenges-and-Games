cc8a752b4270cc6434fbcee25b6ad17327f95309
Aditya Singh <aditya.singh27920@gmail.com>
2023-12-16 15:15:40 +0530

    Loops

--- Notes/Loops.js
+++ Notes/Loops.js
@@ -53,10 +53,49 @@ return result
 console.log(Max([1,2,3,4,5,6,7,20,5,7,975,348,2628,72,272372,983289]));
 
 //letter frequency
-const letterFrequency = (phrase) => {
-    let freq = 0;
-    for(word of phrase){
-        
-    }
+const LetterFreq = (phrase) => {
+  //it'll print the data in phrase
+  console.log(phrase);
+  let Frequency = {};
 
-}
+  for (const letter of phrase) {
+       //print letters one by one in the phrase
+  //   console.log(letter);
+
+     //check if letter exist in frequency
+    if (letter in Frequency) {
+      //it'll increase it's value by one
+      Frequency[letter] += 1;
+    } else {
+       //otherwise, set the value to 1
+      Frequency[letter] = 1;
+    }
+  }
+
+  return Frequency;
+};
+console.log(LetterFreq('Lol, what are you doing tonight?, I hope you are doing fine.'));
+
+//Incremental Operators
+// ++, --, +=
+
+//word frequency in a sentence
+const WordFrequency = (phrase) => {
+  let Frequency = {};
+  const words = phrase.split(" ");
+  for (const word of words) {
+    console.log(word);
+
+    if (word in Frequency) {
+      Frequency[word] = +1;
+    } else {
+      Frequency[word] = 1;
+    }
+  }
+  return Frequency;
+};
+console.log(
+  WordFrequency(
+    "The curious cat chased a fluttering butterfly through the sunlit garden."
+  )
+);
